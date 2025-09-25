d2792575be8fffec6cc3e31b3387344e5a9dc0ac
pretender-2905 <pretender2905@gmail.com>
2025-09-25 15:05:02 +0500

    conmmit

--- STEP_01/LEC_02/PATTERNS/paterns.cpp
+++ STEP_01/LEC_02/PATTERNS/paterns.cpp
@@ -47,6 +47,34 @@ void print5(int n ){
         cout << endl;
     }
 }
+
+void print6(int n){
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n-i+1; j++){
+            cout << j;
+        }
+        cout << endl;
+    }
+}
+
+void print7(int n){
+    for(int i=0; i<n; i++){
+        // space
+        for(int j=0; j<n-i-1; j++){
+            cout<< " ";
+        }
+        // stars
+
+        for(int j=0; j<2 * i + 1; j++){
+            cout<< "*";
+        }
+        // space
+        for(int j=0; j<n-i-1; j++){
+            cout<< " ";
+        }
+        cout<< endl;
+    }
+}
 int main(){
     int n;
     cin >> n;
@@ -54,5 +82,7 @@ int main(){
     // print2(n);
     // print3(n);
     // print4(n);
-    print5(n);
+    // print5(n);
+    // print6(n);
+    print7(n);
 }
